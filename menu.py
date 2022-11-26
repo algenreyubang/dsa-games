5cf5285c53dfdf2ed6d69001db9bb9740b406331
algenreyubang <117018916+algenreyubang@users.noreply.github.com>
2022-11-26 12:59:35 +0800

    Add files via upload
    hello

---
+++ menu.py
@@ -0,0 +1,60 @@
+
+ 
+def retuner():
+    print("if you want to go back to the menu press 0 but your game will be reset")
+    print("options\n[1] dadada  \n[0] return to the options ")
+    selection_input = str(input())
+   
+    if selection_input == '0':
+        print(menu())
+    else: 
+        selection_input == '1'
+        print("datadta")
+
+ 
+def secondretuner():
+    print("if you want to go back to the menu press 0")
+    print("options\n[1] dadada  \n[0] return to the options ")
+    selection_input = str(input())
+   
+    if selection_input == '0':
+        print(menu())
+    else: 
+        selection_input == '1'
+        print("datadta")       
+
+def setthings():
+    print("sethings \n[0] return to options  \n[1] something   \n[2] enter to options ")
+    
+    selection_input = str(input())
+    if selection_input == '2':
+        print("your in the options")
+        print(secondretuner())
+    elif selection_input == '1':
+        print("konwari may volume, tas adjust grapics at brightness")  
+        print(secondretuner())
+        
+    elif selection_input == '0':
+        print(secondretuner())    
+
+    
+
+def menu():
+    print("menu \n[1] start \n[2] settings\n[3] how to play\n[4] exit")
+   
+    selection_input = str(input())
+    if selection_input == '1':
+        from stack import stack
+        stack()
+        print(retuner())
+        retuner()
+    elif selection_input == '2':
+        print(setthings())
+    elif selection_input == '3':
+        print("you entered how to play") 
+        secondretuner()  
+        
+    else:
+     selection_input == '4'
+     exit
+menu()            
---
+++ stack.py
@@ -0,0 +1,11 @@
+def stack():
+    print("take a  lokk of this set of number ")
+    print('0')
+    print('2')
+    print('1')
+    print('4')
+    print('3')
+    print('5')
+    print("your task is to stack them in corect order form 0 to 5")
+    
+stack()
